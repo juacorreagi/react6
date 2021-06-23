309a07a33739c125d158d3649b5f617036a2e79e
juacorreagi <juacorreagi94@gmail.com>
2021-06-22 20:47:23 -0500

    Mostrando lista

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+
+/// Mostrar una lista en base a un arreglo
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      list: ['Javascript', 'CSS', 'HTLM', 'React']
+    }
+  }
+  render() {
+    return (
+      <div style={styles.container}>
+        <h1 style={styles.title}>Las tecnologias del Front</h1>
+        <ul style={styles.list}>
+          {this.state.list.map((prop) =>
+          <li>{ prop }</li>)}
+        </ul>
+      </div>
+    );
+  }
+}
+
+const styles = {
+  container: {
+    height: '100vh',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  title: {
+    marginTop: '100px'
+  },
+  list: {
+    fontSize: '20px',
+    flexGrow: 6
+  }
+}
+
+export default App;
